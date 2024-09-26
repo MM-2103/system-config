9bd2de9e460652f1a722798bb0bb668e0dec8455
MM-2103 <mohsen.menem@protonmail.com>
2024-09-26 09:03:33 +0200

    updated wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,5 +4,22 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- This is where you actually apply your config choices
+config = {
+	color_scheme = "Kanagawa (Gogh)",
+	font = wezterm.font("IosevkaTerm Nerd Font Mono"),
+	font_size = 14.0,
+	default_cursor_style = "SteadyBar",
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	window_decorations = "NONE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
 -- and finally, return the configuration to wezterm
 return config
