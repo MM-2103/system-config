e1115085bbfde18fd7d4f0a38a0ef83c47457f24
MM-2103 <mohsen.menem@protonmail.com>
2024-10-10 17:31:31 +0200

    Added a function for easy file extraction and another one for making and cd'ing to a directory

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,46 @@ if status is-interactive
 	
 ## Make all dots into cd ../
 function multicd
-	echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+
+function take
+    mkdir -p $argv && cd $argv
+end
+
+# Function to extract files
+function extract
+    echo Extracting $argv ...
+    if test -f $argv[1]
+        switch $argv[1]
+            case '*.tar.bz2'
+                tar xjf $argv[1]
+            case '*.tar.gz'
+                tar xzf $argv[1]
+            case '*.bz2'
+                bunzip2 $argv[1]
+            case '*.rar'
+                tar xzf $argv[1]
+            case '*.gz'
+                gunzip $argv[1]
+            case '*.tar'
+                tar xf $argv[1]
+            case '*.tbz2'
+                tar xjf $argv[1]
+            case '*.tgz'
+                tar xzf $argv[1]
+            case '*.zip'
+                unzip $argv[1]
+            case '*.Z'
+                uncompress $argv[1]
+            case '*.7z'
+                7z x $argv[1]
+            case '*'
+                echo "'$argv[1]' cannot be extracted via extract()"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
 end
 
 ## Change prompt color in SSH
