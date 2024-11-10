ac398949e7e9e647af7474bc487a6fa6872825bd
MM-2103 <mohsen.menem@protonmail.com>
2024-11-10 18:46:42 +0100

    Added awesomewm config to this repo

---
+++ awesome/README.md
@@ -0,0 +1,4 @@
+# Awesome-Config-Archlinux
+My awesome config for arch based distros
+
+This is so I don't lose my config file in case I distro hop, feel free to copy it if you want but I would advise against it, since it contains nothing special and you could probably find better dotfiles to copy
---
+++ awesome/autostart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+xfce4-clipman &
+nm-applet &
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+kdeconnectd &
+kdeconnect-indicator &
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
+xfce4-power-manager &
+sleep 1
+autorandr -c
+feh --bg-scale ~/Pictures/Wallpaper/wallpaper-02-Ys--The-Oath-in-Felghana-2560x1440.jpg
+picom --config ~/.config/picom/picom.conf &
