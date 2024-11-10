4ad800f33a3b19f8eccaf9b4eac4344bf3fddf94
MM-2103 <mohsen.menem@protonmail.com>
2024-11-10 18:50:47 +0100

    Added i3wm

---
+++ i3/autostart.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+autorandr -c
+xfce4-clipman &
+nm-applet &
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+dunst -conf "~/.config/dunst/dunstrc" &
+kdeconnectd &
+kdeconnect-indicator &
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
+xfce4-power-manager &
+pipewire &
+feh --bg-scale ~/Pictures/Wallpaper/wallpaper-02-Ys--The-Oath-in-Felghana-2560x1440.jpg
+picom --config ~/.config/picom/picom.conf
