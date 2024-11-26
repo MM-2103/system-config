d80500f53ac6a58527ffc47171c6e88f7dd60753
MM-2103 <mohsen.menem@protonmail.com>
2024-11-26 18:50:19 +0100

    Path changes

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -61,7 +61,7 @@ return {
       sections = {
         {
           section = 'terminal',
-          cmd = 'chafa ~/Pictures/unicorn.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
+          cmd = 'chafa ~/Pictures/Saved/unicorn.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
           height = 17,
           padding = 1,
         },
