d40eed5268d9c03a2b8b670e0a11ec73033cd8ca
MM-2103 <mohsen.menem@protonmail.com>
2025-08-05 21:47:53 +0200

    home-manager: some clean ups and added most of my settings from NixOS

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,185 +1,129 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 
 {
-  nixGL = {
-    vulkan.enable = true;
-    packages = pkgs.nixgl;
-    defaultWrapper = "mesa";
-    offloadWrapper = "nvidiaPrime";
-    installScripts = [
-      "mesa"
-      "nvidiaPrime"
-    ];
-  };
+  # -------- Tell Home-Manager we are NOT on NixOS --------------------
+  targets.genericLinux.enable = true;
 
+  # -------- Basic user metadata -------------------------------------
+  home.username      = "mm-2103";
+  home.homeDirectory = "/home/mm-2103";
+  home.stateVersion  = "25.05";
+
+  # If you want unfree software from Nixpkgs
   nixpkgs.config.allowUnfree = true;
 
-  home.username = "mm-2103";
-  home.homeDirectory = "/home/mm-2103";
-  home.stateVersion = "25.05";
-
+  # ------------------------------------------------------------------
+  # Packages (formerly both environment.systemPackages + home.packages)
+  # ------------------------------------------------------------------
   home.packages = with pkgs; [
-    lazygit
-    lazydocker
-    ansible-language-server
-    antares
-    eza
-    protonvpn-gui
-    sesh
-    television
-    umu-launcher
-    evil-helix
-    dysk
-    thunderbird
-    intelephense
-    gimp3
-    mako
-    protonup-qt
-    pandoc
-    atuin
-    texliveSmall
-    sqlite
-    typora
-    dust
-    tealdeer
-    fish
-    zoxide
-    metasploit
-    jq
-    yq
-    rustup
-    cliphist
-    go
-    php
-    php84Packages.composer
-    bun
-    htop
-    pamixer
-    zellij
-    btop
-    cmake
-    prettierd
-    nodePackages.prettier
-    ruby
-    git-crypt
-    zig
-    ly
-
-    # Nerd fonts
-    nerd-fonts.iosevka
-    nerd-fonts.iosevka-term
-
-    # Install Iosevka font
-    # (nerdfonts.override { fonts = [ "Iosevka" ]; })
-
-    # Packages that need OpenGL/Vulkan - wrapped automatically
-    # (config.lib.nixGL.wrap zed-editor-fhs)
-    (config.lib.nixGL.wrap freecad-wayland)
+    lazygit lazydocker ansible-language-server antares eza protonvpn-gui
+    sesh television umu-launcher evil-helix dysk thunderbird intelephense
+    gimp3 protonup-qt pandoc atuin texliveSmall sqlite dust tealdeer zoxide
+    metasploit jq yq rustup cliphist go php bun htop pamixer zellij btop
+    cmake prettierd ruby git-crypt zig ly tmux fzf networkmanagerapplet
+    nwg-look mesa-demos unigine-heaven mangohud protonup heroic git-extras
+    delta xivlauncher keepassxc adw-gtk3 libsForQt5.qt5ct gnome-themes-extra
+    opencode chromium sgdboop fastfetch mpv bat rubyPackages.solargraph
+    typescript-language-server omnisharp-roslyn libreoffice-qt6-fresh gh
+    ripgrep git-lfs discord pavucontrol hyprlock hypridle hyprpaper
+    hyprpicker hyprsunset tor-browser librewolf lutris calibre
+    rocmPackages.llvm.clang-tools obs-studio
   ];
 
-  home.file = {
-    # ~/.config/ destinations
-    # ".config/alacritty".source = ../dotfiles/alacritty;
-    #    ".config/atuin".source = ../dotfiles/atuin;
-    #".config/awesome".source = ../dotfiles/awesome;
-    #".config/bspwm".source = ../dotfiles/bspwm;
-    #".config/dunst".source = ../dotfiles/dunst;
-    #".config/emacs".source = ../dotfiles/emacs;
-    #".config/eza".source = ../dotfiles/eza;
-    #".config/fastfetch".source = ../dotfiles/fastfetch;
-    #".config/fish".source = ../dotfiles/fish;
-    #".config/foot".source = ../dotfiles/foot;
-    #".config/fuzzel".source = ../dotfiles/fuzzel;
-    #".config/ghostty".source = ../dotfiles/ghostty;
-    #".config/helix".source = ../dotfiles/helix;
-    #".config/hypr".source = ../dotfiles/hypr;
-    #".config/i3".source = ../dotfiles/i3;
-    #".config/kitty".source = ../dotfiles/kitty;
-    #".config/lazygit".source = ../dotfiles/lazygit;
-    #".config/mako".source = ../dotfiles/mako;
-    #".config/niri".source = ../dotfiles/niri;
-    #".config/nix".source = ../dotfiles/nix;
-    #".config/nushell".source = ../dotfiles/nushell;
-    #".config/nvim".source = ../dotfiles/nvim;
-    #".config/picom".source = ../dotfiles/picom;
-    #".config/quickshell".source = ../dotfiles/quickshell;
-    #".config/river".source = ../dotfiles/river;
-    #".config/rofi".source = ../dotfiles/rofi;
-    #".config/sesh".source = ../dotfiles/sesh;
-    #".config/starship".source = ../dotfiles/starship;
-    #".config/swaylock".source = ../dotfiles/swaylock;
-    #".config/tmux".source = ../dotfiles/tmux;
-    #".config/uwsm".source = ../dotfiles/uwsm;
-    #".config/waybar".source = ../dotfiles/waybar;
-    #".config/wayfire".source = ../dotfiles/wayfire;
-    #".config/wezterm".source = ../dotfiles/wezterm;
-    #".config/wireplumber".source = ../dotfiles/wireplumber;
-    #".config/xmonad".source = ../dotfiles/xmonad;
-    #".config/zsh".source = ../dotfiles/zsh;
-
-    # ~/ destinations (traditional locations)
-    #".tmux.conf".source = ../dotfiles/tmate/tmate.conf;  # If you want tmate config as tmux
-
-    # Scripts and other files
-    # ".local/bin/dwm-autostart".source = ../dotfiles/dwm/autostart.sh;
-    # ".local/bin/gnome-startup".source = ../dotfiles/gnome/startup.sh;
-    # ".local/bin/plasma-autostart".source = ../dotfiles/plasma/autostart.sh;
-    # ".local/bin/i3-autostart".source = ../dotfiles/i3/autostart.sh;
-
-    # Distrobox images (if you want them accessible)
-    # ".local/share/distrobox-images".source = ../dotfiles/distrobox-images;
-  };
-
+  # ------------------------------------------------------------------
+  # Session-wide environment
+  # ------------------------------------------------------------------
   home.sessionVariables = {
-    EDITOR = "nvim";
+    GTK_THEME            = "adw-gtk3-dark";
+    QT_STYLE_OVERRIDE    = "breeze";
+    XDG_CURRENT_DESKTOP  = "gnome";
+    EDITOR               = "nvim";
   };
 
-  programs = {
-    home-manager.enable = true;
+  # ------------------------------------------------------------------
+  # Theming
+  # ------------------------------------------------------------------
+  gtk = {
+    enable = true;
+    theme = {
+      name    = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+    iconTheme = {
+      name    = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+  };
 
+  qt = {
+    enable = true;
+    style.name = "breeze";
+  };
+
+  # ------------------------------------------------------------------
+  # MIME defaults
+  # ------------------------------------------------------------------
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf"        = "org.kde.okular.desktop";
+      "image/jpeg"             = "org.kde.gwenview.desktop";
+      "image/png"              = "org.kde.gwenview.desktop";
+      "video/mp4"              = "mpv.desktop";
+      "video/x-matroska"       = "mpv.desktop";
+      "x-scheme-handler/http"  = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+    };
+    associations.added = {
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+    };
+  };
+
+  # ------------------------------------------------------------------
+  # Dot-files (uncomment what you actually want to deploy)
+  # ------------------------------------------------------------------
+  home.file = {
+    # ".config/alacritty".source = ../dotfiles/alacritty;
+  };
+
+  # ------------------------------------------------------------------
+  # User-level programs
+  # ------------------------------------------------------------------
+  programs = {
     git = {
-      enable = true;
-      userName = "mm-2103";
-      userEmail = "mohsen.menem@protonmail.com";
+      enable      = true;
+      userName    = "mm-2103";
+      userEmail   = "mohsen.menem@protonmail.com";
     };
 
     starship.enable = true;
 
     direnv = {
-      enable = true;
+      enable           = true;
       nix-direnv.enable = true;
     };
 
-    emacs = {
-      enable = true;
-    };
+    emacs.enable   = true;
+    fish.enable    = true;
+    tmux.enable    = true;
+    fuzzel.enable  = true;
+    waybar.enable  = true;
   };
 
+  # ------------------------------------------------------------------
+  # User services
+  # ------------------------------------------------------------------
   services = {
     cliphist = {
-      enable = true;
-      allowImages = true;
+      enable       = true;
+      allowImages  = true;
     };
-  };
-
-  fonts.fontconfig = {
-    enable = true;
-    defaultFonts = {
-      monospace = [ "Iosevka Nerd Font Mono" ];
-    };
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    config.common = {
-      default = [ "gtk" ];
-      "org.freedesktop.impl.portal.Secret" = [ "kwalletd6" ];
+    mako.enable       = true;
+    polkit-gnome.enable = true;
+    wlsunset = {
+      enable   = true;
+      sunrise  = "7:00";
+      sunset   = "23:00";
     };
   };
 }
