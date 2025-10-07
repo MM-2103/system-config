f3451688fd6fcd9b87e58b4703ee9f3e6dcde3e1
MM-2103 <mohsen.menem@protonmail.com>
2025-10-07 20:37:55 +0200

    nvim: added blink.cmp and dutch dictionary for spell checking

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -55,16 +55,16 @@ vim.pack.add({
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
   { src = "https://github.com/tpope/vim-fugitive" },
-  { src = "https://github.com/vimwiki/vimwiki" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
   { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/Saghen/blink.cmp" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 require "mason".setup()
 require "nvim-treesitter".setup {
-  ensure_installed = { "php", "javascript", "lua", "c", "nix", "html", "c_sharp", "go" },
+  ensure_installed = { "php", "javascript", "lua", "c", "nix", "html", "c_sharp", "go", "razor" },
   auto_install = true,
   highlight = {
     enable = true,
@@ -80,7 +80,6 @@ require('mini.basics').setup()
 require('mini.extra').setup()
 require('mini.icons').setup()
 require('mini.comment').setup()
-require('mini.completion').setup()
 require('mini.statusline').setup()
 require('mini.tabline').setup()
 require('mini.starter').setup()
@@ -123,6 +122,44 @@ require('mason-lspconfig').setup()
 
 require('which-key').setup {
   preset = "helix",
+}
+
+require('blink.cmp').setup {
+  -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  -- 'super-tab' for mappings similar to vscode (tab to accept)
+  -- 'enter' for enter to accept
+  -- 'none' for no mappings
+  --
+  -- All presets have the following mappings:
+  -- C-space: Open menu or open docs if already open
+  -- C-n/C-p or Up/Down: Select next/previous item
+  -- C-e: Hide menu
+  -- C-k: Toggle signature help (if signature.enabled = true)
+  --
+  -- See :h blink-cmp-config-keymap for defining your own keymap
+  keymap = { preset = 'default' },
+
+  appearance = {
+    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- Adjusts spacing to ensure icons are aligned
+    nerd_font_variant = 'mono'
+  },
+
+  -- (Default) Only show the documentation popup when manually triggered
+  completion = { documentation = { auto_show = true } },
+
+  -- Default list of enabled providers defined so that you can extend it
+  -- elsewhere in your config, without redefining it, due to `opts_extend`
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+
+  -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --
+  -- See the fuzzy documentation for more information
+  fuzzy = { implementation = "prefer_rust_with_warning" }
 }
 
 -- Colorscheme --
