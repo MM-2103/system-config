a572d0a3e0ad71bd659e096c3043c26b4bad2a35
MM-2103 <mohsen.menem@protonmail.com>
2025-12-07 01:51:12 +0100

    nvim: update keybindings

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -44,6 +44,7 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
 vim.keymap.set('n', 'H', ':bprevious<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'L', ':bnext<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-z>", "<Nop>", { silent = true })
 
 -- Plugins --
 vim.pack.add({
