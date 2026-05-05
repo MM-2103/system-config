a68edfc1803b70342a0f529f9b633b43db289ede
MM-2103 <mohsen.menem@protonmail.com>
2026-05-06 00:29:07 +0200

    niri: updates to use quickshell

--- dotfiles/niri/scripts/cliphist.sh
+++ dotfiles/niri/scripts/cliphist.sh
@@ -1,3 +1,5 @@
 !#/usr/bin/env bash
 
-cliphist list | fuzzel --dmenu | cliphist decode | wl-copy
+# cliphist list | fuzzel --dmenu | cliphist decode | wl-copy
+
+qs -p /home/mm-2103/projects/personal/quickshell-bar ipc call clipboard open
